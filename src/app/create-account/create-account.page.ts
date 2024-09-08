a58ec33fa831250a85711732093846e2f2bfc3f4
geumjue <whkrmawn@naver.com>
2024-09-08 14:22:04 +0900

    회원가입 페이지

--- src/app/create-account/create-account.page.ts
+++ src/app/create-account/create-account.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import{ AuthService } from "../services/auth.service";
+
 
 @Component({
   selector: 'app-create-account',
@@ -7,16 +9,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-account.page.scss'],
 })
 export class CreateAccountPage implements OnInit {
+  nickname: string = '';
+  email: string ='';
+  password:string='';
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
   }
 
   ngOnInit() {
-    this.loadData();
   }
 
-  loadData() {
-
+  signUp() {
+    if (this.nickname && this.email && this.password) {
+      this.authService.signUp(this.nickname, this.email, this.password).subscribe(
+        () => {
+          console.log('회원가입 성공');
+          this.router.navigate(['/tabs/tab1']); // 회원가입 후 첫 번째 탭으로 이동
+        },
+        (error) => {
+          console.error('회원가입 실패:', error);
+        }
+      );
+    } else {
+      console.error('모든 필드를 입력하세요.');
+    }
   }
 
   goBackLoginPage() {

62cf96046561be3f458a6f4147565b99a7460d7a
gold_zooo <whkrmawn@naver.com>
2024-09-25 18:51:41 +0900

    collection page 틀 생성, 예상 컬렉션 생성

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'create-account',
+    loadChildren: () => import('./auth/create-account/create-account.module').then(m => m.CreateAccountPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./layout/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'mypage',
+    loadChildren: () => import('./mypage/mypage.module').then(m => m.MypagePageModule)
+
+  },
+  {
+    path: 'nows-comment',
+    loadChildren: () => import('./comment/nows-comment/nows-comment.module').then(m => m.NowsCommentPageModule)
+  },
+
+  {
+    path: 'movie-detail',
+    loadChildren: () => import('./movie/movie-detail/movie-detail.module').then(m => m.MovieDetailPageModule)
+  },
+  {
+    path: 'comment-write',
+    loadChildren: () => import('./comment/comment-write/comment-write.module').then(m => m.CommentWritePageModule)
+  },
+  {
+    path: 'collection',
+    loadChildren: () => import('./collection/collection.module').then( m => m.CollectionPageModule)
+  },
+
+
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}

4158c0a3a54bb63f875f2eb3701dcacf3fe822ed
jimin <minp4028@gmail.com>
2024-09-28 15:39:26 +0900

    modal 기능 수정합니다

--- src/app/movie/movie-detail/movie-detail.page.ts
+++ src/app/movie/movie-detail/movie-detail.page.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { personCircle } from 'ionicons/icons';
 
+
+
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.page.html',
@@ -10,17 +12,12 @@ import { personCircle } from 'ionicons/icons';
 })
 export class MovieDetailPage implements OnInit {
 
-  isModalOpen = false; // Modal 열림 상태
+  isModalOpen = false;  // Modal 열림 상태
+  selectedImage: string | null = null; // 선택된 이미지 경로 저장
 
-  // Modal을 열기 위한 메서드
-  presentModal() {
-    this.isModalOpen = true; // Modal 열림
-  }
 
-  // Modal을 닫기 위한 메서드
-  closeModal() {
-    this.isModalOpen = false; // Modal 닫힘
-  }
+
+
 
   isGalleryOpen = true;
   //슬라이더 옵션 설정
@@ -32,14 +29,25 @@ export class MovieDetailPage implements OnInit {
   constructor(private router: Router) {
     addIcons({ personCircle });
 
-
-
-
   }
 
   ngOnInit() {
     this.loadData()
+
   }
+  // Modal을 열기 위한 메서드
+  presentModal(imageUrl: string) {
+    this.selectedImage = imageUrl; // 클릭된 이미지의 URL을 저장
+    this.isModalOpen = true; // Modal 열림
+  }
+
+  // Modal을 닫기 위한 메서드
+  closeModal() {
+    this.isModalOpen = false; // Modal 닫힘
+    this.selectedImage = null; // 선택된 이미지 초기화
+  }
+
+
   loadData() {
 
   }

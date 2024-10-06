aa55912012340d46ca70c1cb5a76e6536c36a344
kdk1248 <alice3976@g.hongik.ac.kr>
2024-10-06 11:11:47 +0900

    영화 디테일 페이지와 좋아요 페이지 연결

--- src/app/pages/movie/movie-routing.module.ts
+++ src/app/pages/movie/movie-routing.module.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { SearchPage } from './movie-search/search.page';
 import { MovieDetailPage } from './movie-detail/movie-detail.page';
 import { CommentWritePage } from './movie-comment/comment-write/comment-write.page';
+import { MovieFavoritePage } from './movie-favorite/movie-favorite.page';
 
 const routes: Routes = [
   {
     path: '',
     component: SearchPage
   },
+  {
+    path: '',
+    component: MovieFavoritePage
+  },
+  
   {
     path: 'detail/:id',
     component: MovieDetailPage // 영화 상세 페이지 컴포넌트

56ab1d85b25f671219c6af44d8da9b4e3cb020b6
gold_zooo <whkrmawn@naver.com>
2024-10-09 14:46:11 +0900

    movieCharater 페이지 generate

---
+++ src/app/pages/movie/movie-character/movie-character.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-movie-character',
+  templateUrl: './movie-character.page.html',
+  styleUrls: ['./movie-character.page.scss'],
+})
+export class MovieCharacterPage implements OnInit {
+
+  constructor(private route: Router, private activateRoute: ActivatedRoute) { }
+
+
+  goBackMovieDetailPage() {
+    this.route.navigate(['/movie/detail']);
+  }
+
+  ngOnInit() {
+  }
+
+}

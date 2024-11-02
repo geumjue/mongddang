f2ed6aa34732b3938865e85ec5081ef6abb41dab
geumjue <whkrmawn@naver.com>
2024-11-02 14:55:12 +0900

    SearchPage; search바로 들어가서 default 검색어 뜨는 기능 적용

--- src/app/pages/movie/movie-search/search.page.ts
+++ src/app/pages/movie/movie-search/search.page.ts
@@ -1,27 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GetMovieByIdResponseData } from "../../../models/movie/movie-getmoviebyid-response-data.interface";
 import { MovieService } from '../../../services/movie/movie.service';
-import { GetMoviesResponseData } from '../../../models/movie/movie-getmovie-response-data.interface'; // 영화 데이터 인터페이스 가져오기
+import { GetMoviesResponseData } from '../../../models/movie/movie-getmovie-response-data.interface';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss']
 })
-export class SearchPage {
+export class SearchPage implements OnInit {
   searchTerm: string = '';
   searchResults: string[] = [];
   data: string[] = []; // 영화 제목 배열
   movie: GetMovieByIdResponseData | undefined; // 선택된 영화 정보를 저장하기 위한 변수
 
-  constructor(private router: Router, private movieService: MovieService) {
-    // HomePage에서 전달된 상태를 가져옵니다.
-    const navigation = this.router.getCurrentNavigation();
-    if (navigation?.extras?.state?.['movies']) { // 대괄호 표기법 사용
-      const movies: GetMoviesResponseData[] = navigation.extras.state['movies']; // 대괄호 표기법 사용
-      this.data = movies.map(movie => movie.title); // 영화 제목 배열 초기화
-    }
+  constructor(private router: Router, private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.loadMovies(); // 컴포넌트 초기화 시 영화 데이터 로드
+  }
+
+  loadMovies() {
+    this.movieService.getMovies().subscribe({
+      next: (response: GetMoviesResponseData[]) => {
+        this.data = response.map(movie => movie.title); // 영화 제목 배열 초기화
+      },
+      error: (err: any) => {
+        console.log(err);
+      }
+    });
   }
 
   getMovieById(id: string) {
